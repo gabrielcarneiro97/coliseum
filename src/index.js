23a721f35c51c0506da128c86dee0c28f2be1ed0
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-06-21 11:37:08 -0300

    Webpack added

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export function teste() {
+  console.log('teste');
+}
+
+export function teste2() {
+  console.log('teste2');
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+
+const nodeModules = {};
+
+fs.readdirSync('node_modules')
+  .filter(x => ['.bin'].indexOf(x) === -1)
+  .forEach((mod) => {
+    nodeModules[mod] = `commonjs ${mod}`;
+  });
+
+module.exports = {
+  entry: './src/index.js',
+  target: 'node',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    library: 'coliseum',
+  },
+  externals: nodeModules,
+  plugins: [
+    new webpack.IgnorePlugin(/\.(css|less)$/),
+  ],
+  mode: 'production',
+};
